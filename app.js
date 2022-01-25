30a07bc198c86481fb537de3002b74431d13996c
Jack Stevens <77250250+Jakalanern@users.noreply.github.com>
2022-01-25 11:38:25 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let burger = document.querySelector(".burger");
 let x = document.querySelector(".x");
 let dropdown = document.querySelector(".dropdown");
+let navbar = document.querySelector("nav");
 
 x.style.display = "none";
 
@@ -8,12 +9,23 @@ burger.addEventListener("click", function () {
   burger.style.display = "none";
   x.style.display = "initial";
 
-  dropdown.style.transform = "translateX(0)";
+  dropdown.style.right = "0";
 });
 
 x.addEventListener("click", function () {
   x.style.display = "none";
   burger.style.display = "flex";
 
-  dropdown.style.transform = "translateX(-2000px)";
+  dropdown.style.right = "-100%";
+});
+
+window.addEventListener("scroll", function () {
+  if (
+    document.body.scrollTop >= 25 ||
+    document.documentElement.scrollTop >= 25
+  ) {
+    navbar.style.background = "black";
+  } else {
+    navbar.style.background = "transparent";
+  }
 });
