84e363e40b8500b36e7a4cc6ce6bf101eae99d88
Jack Stevens <77250250+Jakalanern@users.noreply.github.com>
2022-01-27 16:13:51 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ let navbar = document.querySelector("nav");
 let getStarted = document.querySelector(".get-started");
 let footerNavContent = document.querySelectorAll(".footer-nav-content");
 let footerArrow = document.querySelectorAll(".footer-arrow");
+let yasImg = document.querySelector(".yas-img");
+let sheDoesImg = document.querySelector(".shedoes-img");
+let leeImg = document.querySelector(".lee-img");
+let scandImg = document.querySelector(".scand-img");
+let girlImg = document.querySelector(".girl-img");
+let hotelImg = document.querySelector(".hotel-img");
+let popup1 = document.querySelector(".yas-popup");
+let popup2 = document.querySelector(".shedoes-popup");
+let popup3 = document.querySelector(".lee-popup");
+let popup4 = document.querySelector(".scand-popup");
+let popup5 = document.querySelector(".girl-popup");
+let popup6 = document.querySelector(".hotel-popup");
+let popupX = document.querySelectorAll(".popup-x");
+let overlay = document.querySelector(".overlay");
 
 x.style.display = "none";
 
@@ -73,4 +87,56 @@ footerArrow[4].addEventListener("click", function () {
     f.style.color = "transparent";
   }
   footerNavContent[4].classList.toggle("height-auto");
+});
+
+yasImg.addEventListener("click", function () {
+  popup1.classList.add("is-active");
+  overlay.style.display = "initial";
+});
+
+sheDoesImg.addEventListener("click", function () {
+  popup2.classList.add("is-active");
+  overlay.style.display = "initial";
+});
+
+leeImg.addEventListener("click", function () {
+  popup3.classList.add("is-active");
+  overlay.style.display = "initial";
+});
+
+scandImg.addEventListener("click", function () {
+  popup4.classList.add("is-active");
+  overlay.style.display = "initial";
+});
+
+girlImg.addEventListener("click", function () {
+  popup5.classList.add("is-active");
+  overlay.style.display = "initial";
+});
+
+hotelImg.addEventListener("click", function () {
+  popup6.classList.add("is-active");
+  overlay.style.display = "initial";
+});
+
+for (let x of popupX) {
+  x.addEventListener("click", function () {
+    overlay.style.display = "none";
+    popup1.classList.remove("is-active");
+    popup2.classList.remove("is-active");
+    popup3.classList.remove("is-active");
+    popup4.classList.remove("is-active");
+    popup5.classList.remove("is-active");
+    popup6.classList.remove("is-active");
+  });
+}
+
+overlay.addEventListener("click", function () {
+  overlay.style.display = "none";
+  popup1.classList.remove("is-active");
+  popup2.classList.remove("is-active");
+  popup3.classList.remove("is-active");
+  popup4.classList.remove("is-active");
+  popup5.classList.remove("is-active");
+  popup6.classList.remove("is-active");
 });
